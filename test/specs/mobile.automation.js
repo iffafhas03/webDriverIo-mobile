7012c02bb84e467c8d60cbb9b27557f42cd211de
Iffa Khalisha <iffakhalisa99@gmail.com>
2023-07-09 20:05:54 +0700

    tugas akhir mobile automation

---
+++ test/specs/mobile.automation.js
@@ -0,0 +1,19 @@
+describe("Open Application", () => {
+  // it('should open apps', async() => {
+  //     await $("//android.widget.TextView[@content-desc='Accessibility']").click();
+  //     deleteSession();
+
+  // })
+  it("should open apps", async () => {
+    // await $(
+    //   "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout[1]/android.widget.FrameLayout/android.widget.EditText"
+    // ).click();
+    await $(
+      "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout[1]/android.widget.FrameLayout/android.widget.EditText"
+    ).type("ica");
+    deleteSession();
+  });
+});
+///hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout[1]/android.widget.FrameLayout/android.widget.EditText
+
+//menit 24
